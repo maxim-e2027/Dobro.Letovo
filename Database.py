39eb522944eca0ea2399a894cc1d3a7d091cb464
maxim-e2027 <maximpaleo@gmail.com>
2025-10-02 23:32:01 +0300

    Add files via upload
    Databse creation file updated with registration and hash-protected personal data tables

--- Database.py
+++ Database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+import bcrypt
 
 DATABASE_URL = "sqlite:///DobroLetovo.db"
 engine = create_engine(DATABASE_URL, echo=True)
@@ -27,6 +28,41 @@ class Organisations(Base):
 
     def __repr__(self):
         return f"<Organisation(ID={self.Organisation_ID_ID}, name={self.Organisation_Name_Name}, Description={self.Organisation_Description})>"
+class Volunteer_Autentification(Base):
+    __tablename__ = 'Volunteer_Autentification'
+    Volunteer_ID = Column(Integer, primary_key=True)
+    Volunteer_Username = Column(String(128), nullable=False)
+    Volunteer_Password_Hash = Column(String(128), nullable=False)
+    # Organisation_Description contains not the description itself, but the path to the file containing it
+    Organisation_Description = Column(String(20))
+
+    def __repr__(self):
+        return f"<Volunteer(ID={self.Organisation_ID_ID})>"
+
+    def set_password(self, password: str):
+        self.Volunteer_Password_Hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), self.Volunteer_Password_Hash.encode('utf-8'))
+
+class Organzation_Autentification(Base):
+    extend_existing = True
+    __tablename__ = 'Organization_Autentification'
+    Organization_ID = Column(Integer, primary_key=True)
+    Organization_Username = Column(String(128), nullable=False)
+    Organization_Password_Hash = Column(String(128), nullable=False)
+    # Organisation_Description contains not the description itself, but the path to the file containing it
+    Organisation_Description = Column(String(20))
+
+    def __repr__(self):
+        return f"<Organisation(ID={self.Organisation_ID})"
+
+    def set_password(self, password: str):
+        self.Organization_Password_Hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(password.encode('utf-8'), self.Organization_Password_Hash.encode('utf-8'))
+
 
 class Comments(Base):
     __tablename__ = 'Comments'
@@ -63,11 +99,48 @@ class Requests(Base):
     def __repr__(self):
         return f"<Request(ID={self.Request_ID}, Volunteer={self.Volunteer_ID}, Event={self.Event_ID}, Status={self.Request_Status})>"
 
+def register_volunteer(username: str, password: str, name: str, description: str):
+    if session.query(Volunteer_Autentification).filter_by(username=username).first():
+        print("Username already exists.")
+        return "Username already exists"
+
+    last_id = session.query(Volunteer_Autentification).filter_by(Volunteer_ID).first()
+    user = Volunteer_Autentification(Volunteer_ID = last_id + 1, username=username)
+    user.set_password(password)
+    session.add(user)
+
+    user = Volunteer(Volunteer_ID = last_id + 1, Volunteer_Name = name, Volunteer_Description = description, Volunteer_Rating = 100, Volunteer_Hours = 0)
+    session.add(user)
+    session.commit()
+    print(f"User '{username}' registered successfully.")
+
+def register_organization(username: str, password: str, name: str, description: str):
+    if session.query(Organzation_Autentification).filter_by(username=username).first():
+        print("Username already exists.")
+        return "Username already exists"
+    last_id = session.query(Organzation_Autentification_Autentification).filter_by(Organization_ID).first()
+    user = Organzation_Autentification_Autentification(Organization_ID = last_id + 1, username=username)
+    user.set_password(password)
+    session.add(user)
+
+    user = Volunteer(Organization_ID = last_id + 1, Organization_Name = name, Organization_Description = description)
+    session.add(user)
+    session.commit()
+    print(f"User '{username}' registered successfully.")
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+new_volunteer = Volunteers(
+    Volunteer_ID=4,
+    Volunteer_Name="Petr",
+    Volunteer_Rating=90,
+    Volunteer_Hours=404,
+    Volunteer_Description="path/to/description.txt"
+)
+#session.add(new_volunteer)
 session.commit()
 
-volunteer = session.query(Volunteers).filter_by(Volunteer_Name="Ivan Ivanovich Ivanov").all()
+volunteer = session.query(Volunteers).filter_by(Volunteer_Hours = 404).all()
 print(volunteer)
+
